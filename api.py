a44a8a1b5a194ebc26aad0b6c6eebd43549de7bc
MauriceLim <53830626+MauriceLim@users.noreply.github.com>
2023-12-23 07:32:47 -0500

    Add files via upload

--- api.py
+++ api.py
@@ -1,103 +1,199 @@
-# api.py
+"""
+API Routes for Zen Nilpferd
+"""
+import os
 from typing import Optional
-from pydantic import BaseModel
+
 from fastapi import FastAPI, HTTPException
-from datetime import datetime, timedelta
-import yfinance as yf
-from bs import MyBlackScholes  
+from fastapi.responses import JSONResponse
+from starlette.middleware import Middleware
+from starlette.middleware.base import BaseHTTPMiddleware
+from mysql.connector import MySQLConnection, Error as MySQLError
+
+import models
 
 app = FastAPI()
 
-class MyBlackScholesResponse(BaseModel):
-    stock_symbol: str  
-    is_call_option: bool  
-    interest_rate: float  
-    dividend_rate: float  
-    volatility: float  
-    time_to_expiry: float  
-    observation_date: Optional[str]
-    result: dict
+class BasicChecksMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI, conn: MySQLConnection):
+        super().__init__(app)
+        self._conn = conn
 
-def get_stock_price_new(stock_symbol: str, observation_date: str):
-    ticker = yf.Ticker(stock_symbol)
+    async def dispatch(self, request, call_next):
+        if not self._conn.is_connected():
+            try:
+                # We expect the environment variable MYSQL_HOST will to be
+                # present if we're running within a container.
+                host = os.environ.get("MYSQL_HOST", "127.0.0.1")
+                # Port is always the within container network default port, even
+                # if another, e.g. 3307, is exposed to the Docker host.
+                port = 3306
+                print(f"Connecting to host '{host}', port '{port}'")
+                self._conn.connect(host=host, port=port, user="root",database='zen_nilpferd')
+            except Exception as e:
+                return JSONResponse(
+                    status_code=503, content=f"Database unavailable: {e}"
+                )
 
-    if ticker.history(period="1d").empty:
-        raise HTTPException(status_code=404, detail="Stock symbol not found or Inconsistent Date")
+        result = await call_next(request)
+        # ensure we commit each time to avoid select statement caching
+        self._conn.commit()
+        return result 
+
+
+conn: MySQLConnection = MySQLConnection()
+middleware = [Middleware(BasicChecksMiddleware, conn=conn)]
+app = FastAPI(title="Zen Nilpferd's Quant API", version="0.0.1", middleware=middleware)
+
+# ==========================================================================
+#                            YOUR IMPLEMENTATION
+#                               BELOW HERE!!!
+# NOTES:
+#   - Use the global `conn` variable for the MySQL connection
+# ==========================================================================
+
+
+@app.get("/")
+def root():
+    if conn.is_connected():
+        return "All is well!"
+
+
+@app.get("/assumptions/{id}")
+def assumptions(id: int) -> models.ProjectionAssumptions:
+    cursor = conn.cursor(dictionary=True)
+    try:
+        query = f"SELECT mortality_multiplier, wd_age, min_wd_delay FROM assumptions WHERE id = {id};"
+        
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        # Check if the result is not None (record found)
+        if result is not None:
+            return models.ProjectionAssumptions(**result)
+        else:
+            # Return 404 if the record is not found
+            raise HTTPException(
+                status_code=404,
+                detail="Assumption not found",
+            )
+
+    except MySQLError as e:
+        # Handle database errors
+        return JSONResponse(content={"error": str(e)}, status_code=404)
+
+
+@app.get("/parameters/{id}")
+def parameters(id: int) -> models.ProjectionParameters:
+    cursor = conn.cursor(dictionary=True)
+    try:
+        query = f"SELECT proj_periods, num_paths, seed FROM parameters WHERE id = {id};"
+        
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        # Check if the result is not None (record found)
+        if result is not None:
+            return models.ProjectionParameters(**result)
+        else:
+          
+            raise HTTPException(
+                status_code=404,
+                detail="Parameters not found",
+            )
+
+    except MySQLError as e:
+        # Handle database errors
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def policies(id: Optional[int] = None) -> list[models.PolicyholderRecord]:
+
+    cursor = conn.cursor(dictionary=True)
 
     try:
-        if observation_date is not None:
-            start_date = datetime.strptime(observation_date, "%Y-%m-%d")
-            end_date = start_date + timedelta(days=1)
-
-            data = yf.download(
-                stock_symbol,
-                start=start_date.strftime("%Y-%m-%d"),
-                end=end_date.strftime("%Y-%m-%d")
-            )
+        if id is not None:
+            # Fetch policies for a specific ID
+            query = f"SELECT id, issue_age, initial_premium, fee_pct_av, benefit_type, ratchet_type, guarantee_wd_rate FROM policies WHERE id = {id};"
+        
         else:
-            data = ticker.history(period='1d')
+            # Fetch all policies
+            query = "SELECT id, issue_age, initial_premium, fee_pct_av, benefit_type, ratchet_type, guarantee_wd_rate FROM policies;"
 
-        if data.empty or 'Close' not in data.columns:
-            raise HTTPException(status_code=404, detail="Stock symbol not found or Inconsistent Date")
+        
+        cursor.execute(query)
+        results = cursor.fetchall()
 
-        return data['Close'][0]
+        if id is not None and not results:
+            return []
 
-    except Exception as e:
-        raise HTTPException(status_code=404, detail="Inconsistent Date")
+        all_records = []
+        if results is not None:
+            for result in results:
+                all_records.append(models.PolicyholderRecord(**result))
+            
+        return all_records
 
-@app.get("/hw5/my-black-scholes/{stock_symbol}", response_model=MyBlackScholesResponse)
-def calculate_option_new(stock_symbol: str,
-                     is_call_option: bool,
-                     strike_price: float,  # Change the name of the variable
-                     interest_rate: float,
-                     dividend_rate: float,
-                     volatility: float,
-                     time_to_expiry: float,
-                     observation_date: Optional[str] = None):
-    # get the stock price
-    stock_price = get_stock_price_new(stock_symbol, observation_date)
+    except MySQLError as e:
+        # Handle database errors
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
-    # Create an instance of the MyBlackScholes class
-    option = MyBlackScholes()
+@app.get("/policies")
+def get_policies_without_id():
+    return policies()
 
-    # Call the appropriate methods from the MyBlackScholes class
-    option_price_result = option.calculate_option_price(
-        is_call=is_call_option,
-        S=stock_price,
-        K=strike_price,
-        T=time_to_expiry,
-        r=interest_rate,
-        q=dividend_rate,
-        sigma=volatility
-    )
+@app.get("/policies/{id}")
+def get_policies_with_id(id: int):
+    return policies(id=id)
 
-    # Modify this line to use the greeks method from the MyBlackScholes class
-    greeks_result = option.calculate_greeks(
-        is_call=is_call_option,
-        S=stock_price,
-        K=strike_price,
-        T=time_to_expiry,
-        r=interest_rate,
-        q=dividend_rate,
-        sigma=volatility
-    )
 
-    response = {
-        "stock_symbol": stock_symbol,
-        "is_call_option": is_call_option,
-        "interest_rate": interest_rate,
-        "dividend_rate": dividend_rate,
-        "volatility": volatility,
-        "time_to_expiry": time_to_expiry,
-        "observation_date": observation_date,
-        "result": {
-            "option_price": option_price_result,
-            "greeks": greeks_result
-        }
-    }
+@app.get("/scenario/{id}")
+def scenario(id: int) -> models.ScenarioParameters:
+    
+    cursor = conn.cursor(dictionary=True)
+    
+    try:      
+        query = f"SELECT risk_free_rate, dividend_yield, volatility FROM scenarios WHERE id = {id};"
+        
+        cursor.execute(query)
+        result = cursor.fetchone()
 
-    return response
+        # Check if the result is not None (record found)
+        if result is not None:
+            return models.ScenarioParameters(**result)
+        else:
+            
+            raise HTTPException(
+                status_code=404,
+                detail="Scenarios not found",
+            )
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    except MySQLError as e:
+        # Handle database errors
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+@app.get("/mortality")
+def mortality() -> models.MortalityTable:
+    cursor = conn.cursor(dictionary=True)
+        
+    try:      
+        query = f"SELECT qx FROM mortality;"
+        
+        cursor.execute(query)
+        results = cursor.fetchall()
+        qx_values = [result["qx"] for result in results]
+
+        # Check if the result is not None (record found)
+        if results is not None:
+            return models.MortalityTable(qx=qx_values)
+        else:
+            
+            raise HTTPException(
+                status_code=404,
+                detail="Mortality not found",
+            )
+
+    except MySQLError as e:
+        # Handle database errors
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    ...
